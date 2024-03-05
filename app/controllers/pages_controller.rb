d6e5b69e35e5784e5db116231c9b74eebe64a068
Alexandre Rodriguez <alexandre.rodriguez.arz@gmail.com>
2024-03-05 15:37:24 +0100

    added home action logic to show different buttons if user is signed in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,26 @@
 class PagesController < ApplicationController
-
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    if user_signed_in?
+      @user_is_connected = true
+    else
+      @user_is_connected = false
+    end
+    # TODO: Si utilisateur est connecté redirect to events_
   end
 
   def dashboard
+  end
+
+  private
+
+  def timeout
+    sleep(5)
+    # Attendre 3 secondes
+  end
+  def redirection_to_game_index
+    timeout
+    redirect_to events_path
   end
 end
