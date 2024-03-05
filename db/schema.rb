e9aac4ec916b68d49128ce9cece017311e15b6fa
gabrielle simha <gabrielle.simha@gmail.com>
2024-03-05 10:54:32 +0100

    cloudinary setup NOUBLIEZ PAS DE RAJOUTER MA CLE API DANS VOTRE .ENV

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_04_124626) do
     t.string "name"
     t.text "description"
     t.string "image_url"
-    t.integer "min_players"
+    t.integer "min_player"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
