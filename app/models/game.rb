4f7fd77a01dd2072dfdafe3d251fa04413a99aa4
Alexandre Rodriguez <alexandre.rodriguez.arz@gmail.com>
2024-03-05 01:18:13 +0100

    added has_many events association in models/game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,4 @@
 class Game < ApplicationRecord
   has_many :users, trough: :favorite
-  # Jsuis pas censé touché a ce fichier dans ma branche mais jcrois qu'il y a un has_many :events ici
+  has_many :events
 end
