98d232a643566d3a67f0f347414ecfa62789d463
Alexandre Rodriguez <alexandre.rodriguez.arz@gmail.com>
2024-03-05 12:03:01 +0100

    unfinished parsing XML

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,15 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Game.delete_all
-puts "------------------    ---------------------- "
-puts "               Delete Games"
-puts "------------------    ---------------------- "
-User.delete_all
-puts "------------------    ---------------------- "
-puts "               Delete Users"
-puts "------------------    ---------------------- "
+# Game.delete_all
+# puts "------------------    ---------------------- "
+# puts "               Delete Games"
+# puts "------------------    ---------------------- "
+# User.delete_all
+# puts "------------------    ---------------------- "
+# puts "               Delete Users"
+# puts "------------------    ---------------------- "
+
 # Uno
 # Poker
 # Monopoly
@@ -24,29 +25,45 @@ puts "------------------    ---------------------- "
 # Werewolves
 # Blood on the tower clock
 #
+url = "https://api.geekdo.com/xmlapi/collection/mkgray"
+html_file = URI.open(url).read
+html_doc = Nokogiri::HTML.parse(html_file)
+
+result = []
+html_doc.search("item name, item image, item comment").each do |elements|
+  #Faire une itération sur elements de item pour aller chercher le nom, l'image, le comment et le min players
+  result << element.text.strip
+end
+
+for i in (0..20) do
+  puts result[i]
+end
+
+
+
 
 # seeds.rb
-games = [
-  { name: "Uno", description: "Uno is a classic card game where players aim to be the first to get rid of all their cards by matching colors or numbers.", min_players: 2 },
-  { name: "Poker", description: "Poker is a popular card game that involves betting and strategy, with variations like Texas Hold'em and Omaha.", min_players: 2 },
-  { name: "Monopoly", description: "Monopoly is a classic board game where players buy, sell, and trade properties to become the wealthiest player.", min_players: 2 },
-  { name: "Yu-gi-oh", description: "Yu-Gi-Oh! is a trading card game where players use decks of cards featuring monsters, spells, and traps to duel each other.", min_players: 2 },
-  { name: "Pokemon", description: "Pokemon is a franchise that includes video games, trading card games, and animated series where players catch and train creatures called Pokemon to battle each other.", min_players: 2 },
-  { name: "Werewolves", description: "Werewolves is a party game where players are assigned secret roles as villagers or werewolves, with the villagers trying to identify and eliminate the werewolves.", min_players: 5 },
-  { name: "Blood on the Clock Tower", description: "Blood on the Clock Tower is a mystery board game where players work together to solve a murder that has occurred in a clock tower.", min_players: 5 }
-]
+# games = [
+  # { name: "Uno", description: "Uno is a classic card game where players aim to be the first to get rid of all their cards by matching colors or numbers.", min_players: 2 },
+  # { name: "Poker", description: "Poker is a popular card game that involves betting and strategy, with variations like Texas Hold'em and Omaha.", min_players: 2 },
+  # { name: "Monopoly", description: "Monopoly is a classic board game where players buy, sell, and trade properties to become the wealthiest player.", min_players: 2 },
+  # { name: "Yu-gi-oh", description: "Yu-Gi-Oh! is a trading card game where players use decks of cards featuring monsters, spells, and traps to duel each other.", min_players: 2 },
+  # { name: "Pokemon", description: "Pokemon is a franchise that includes video games, trading card games, and animated series where players catch and train creatures called Pokemon to battle each other.", min_players: 2 },
+  # { name: "Werewolves", description: "Werewolves is a party game where players are assigned secret roles as villagers or werewolves, with the villagers trying to identify and eliminate the werewolves.", min_players: 5 },
+  # { name: "Blood on the Clock Tower", description: "Blood on the Clock Tower is a mystery board game where players work together to solve a murder that has occurred in a clock tower.", min_players: 5 }
+# ]
+#
+# games.each do |game|
+  # Game.create!(name: game[:name], description: game[:description], min_players: game[:min_players])
+# end
+#
+# users = [
+  # { email: "alexandre.rodriguez.arz@gmail.com", password: "123456"},
+  # { email: "theophiledesaintbon@gmail.com", password: "123456"},
+  # { email: "gabrielle.simha@gmail.com", password: "123456"},
+  # { email: "antonindanto@gmail.com", password: "123456"}
+# ]
 
-games.each do |game|
-  Game.create!(name: game[:name], description: game[:description], min_players: game[:min_players])
-end
-
-users = [
-  { email: "alexandre.rodriguez.arz@gmail.com", password: "123456"},
-  { email: "theophiledesaintbon@gmail.com", password: "123456"},
-  { email: "gabrielle.simha@gmail.com", password: "123456"},
-  { email: "antonindanto@gmail.com", password: "123456"}
-]
-
-users.each do |user|
-  User.create!(email: user[:email], password: user[:password])
-end
+# users.each do |user|
+  # User.create!(email: user[:email], password: user[:password])
+# end
