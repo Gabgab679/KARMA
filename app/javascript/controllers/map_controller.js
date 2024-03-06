c38737ec4df61e86e5172c32be75e4d4f5bf94ba
gabrielle simha <gabrielle.simha@gmail.com>
2024-03-06 12:56:25 +0100

    final changes to mapbox + seed à delete ofc

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -19,16 +19,7 @@ export default class extends Controller {
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
   }
-
-  #addMarkersToMap() {
-    this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
-      new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup)
-      .addTo(this.map)
-    })
-  }
+  
 
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
