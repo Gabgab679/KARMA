cc35e098b7855f6437ea76b8597655b27bcb3d62
Alexandre Rodriguez <alexandre.rodriguez.arz@gmail.com>
2024-03-11 16:14:00 +0100

    ignore this commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,6 +6,10 @@ class FavoritesController < ApplicationController
     new_fav.save
   end
 
+  def update
+
+  end
+
   def destroy
     game = Game.find(params[:id])
     game.favorites.each do |favorite|
